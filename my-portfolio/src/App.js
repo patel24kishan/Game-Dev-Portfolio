ee750f20d2d6b617b087ac655030bc2b6a94affb
Kishan Kahodariya <Kishan.Kahodariya@ibm.com>
2024-02-21 13:15:06 -0400

    update styled package

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import {darkTheme} from './utils/Theme';
+
+const Body=styled.div`
+background-color:${({theme})=>theme.bg};
+width:100%;
+overflow-x: hidden;
+`
 
 function App() {
   return (
-    <div className="App">
-      PORTFOLIO
-    </div>
+    <ThemeProvider theme={darkTheme}>
+    <Body>
+      Portfolio
+    </Body>
+    </ThemeProvider>
   );
 }
 
