28afca49ac2fb521e48a98a836ca5a6e18eb68e4
ghosts24 <35022263+patel24kishan@users.noreply.github.com>
2024-02-24 15:06:04 -0400

    update skills

--- my-portfolio/src/components/Skills/index.js
+++ my-portfolio/src/components/Skills/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import {Container, Wrapper, Title, Description, SkillsContainer, Skill, SkillTitle, SkillList, SkillItem, SkillImage} from './SkillStyle';
+import { skills } from "../../data/constants";
+import { Tilt } from "react-tilt";
 
 const Skills=()=>{
-    return (<div>Skills</div>)
+    return ( <Container id="Skills">
+    <Wrapper>
+      <Title>Skills</Title>
+      <SkillsContainer>
+        {skills.map((skill, index) => (
+          <Tilt>
+            <Skill key={`skill-${index}`}>
+              <SkillTitle>{skill.title}</SkillTitle>
+              <SkillList>
+                {skill.skills.map((item, index_x) => (
+                  <SkillItem key={`skill-x-${index_x}`}>
+                    <SkillImage src={item.image} />
+                    {item.name}
+                  </SkillItem>
+                ))}
+              </SkillList>
+            </Skill>
+          </Tilt>
+        ))}
+      </SkillsContainer>
+    </Wrapper>
+  </Container>
+);
 }
 
 export default Skills;
