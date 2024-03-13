7338109e43c1142f745f3762d75835476f7208fc
ghosts24 <35022263+patel24kishan@users.noreply.github.com>
2024-03-13 16:16:20 -0300

    recode errors

--- src/components/Skills/SkillStyle.js
+++ src/components/Skills/SkillStyle.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const Container = styled.div`
 display: flex;
 flex-direction: column;
-justify-content-center;
+justify-content:center;
 position: relative;
 z-index: 1;
 align-items: center;
