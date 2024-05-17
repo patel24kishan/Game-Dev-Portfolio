3a45e4a08df95cb270e8102dff83fc1cc34e142f
ghosts24 <35022263+patel24kishan@users.noreply.github.com>
2024-05-17 12:54:16 -0300

    update latex game dev resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "Creative game developer with one year of experience at India’s leading game studio, with published titles to showcase. Experienced backend developer at IBM Canada's Hybrid Cloud Development team with migrating applications to cloud and proficient in Java, Spring Boot, and Python. Passionate about game mechanics, cloud gaming and problem-solving.",
   github: "https://github.com/patel24kishan",
   resume_gamedeveloper:
-    "https://drive.google.com/file/d/1zl33Ywx3P6TfttfZZMmiQ8OI7hHOSP99/view?usp=drive_link",
+    "https://drive.google.com/file/d/1H_zGKv3Rl-26HxC2E8JINnk__nqCGxu7/view?usp=drive_link",
   resume_softwaredeveloper:
     "https://drive.google.com/file/d/1IDqMwEJxBzXq9XgQcefFmeDTMmLGUlZl/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/kkishan2409/",
